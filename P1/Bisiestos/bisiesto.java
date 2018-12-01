90cf211c695e0fa0b4e3f37c06ddb9a7551cf4a9
rchica <44021420+rchica@users.noreply.github.com>
2018-12-01 12:12:17 +0100

    Update bisiesto.java

--- P1/Bisiestos/bisiesto.java
+++ P1/Bisiestos/bisiesto.java
@@ -1,7 +1,7 @@
 package Bisiesto;
 
 public class bisiesto {
-	public static boolean main(int año) {
+	public static boolean esBisiesto(int año) {
 	     if(año % 4 == 0 && año % 100 != 0 || año % 400 == 0){
 	         return true;
 	     }else{
