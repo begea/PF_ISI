56fe8b9c99612bf0974c64cf16fc9eaaa11908ab
JuanUrena <juanurenagarciagasco@gmail.com>
2018-12-05 02:04:11 +0100

    subida test sobre repo de Lozano

---
+++ malozanom(5)/embotelladora/src/embotelladora/EmbotelladoraTest.java
@@ -0,0 +1,60 @@
+package embotelladora;
+import static org.junit.Assert.*;
+import org.junit.*;
+
+
+
+public class EmbotelladoraTest {
+	
+
+
+	@Test public void allOK()
+	{
+		Embotelladora test= new Embotelladora();
+		assertTrue("Error allOk", 2==test.calculaBotellasPequenas(2,3,17));
+		assertTrue("Error allOk", 0==test.calculaBotellasPequenas(1,5,20));
+		assertTrue("Error allOk", 7==test.calculaBotellasPequenas(9,2,17));
+		assertTrue("Error allOk", 0==test.calculaBotellasPequenas(1,5,0));
+	}
+	
+	@Test public void negativeBig()
+	{
+		try {
+			Embotelladora test= new Embotelladora();
+			assertTrue("Error negativeBig", -1==test.calculaBotellasPequenas(2,-3,17));
+		} catch (Exception e) {
+			return;
+		}
+	}
+	
+	@Test public void negativeLittle()
+	{
+		try {
+			Embotelladora test= new Embotelladora();
+			assertTrue("Error negativeLittle", -1==test.calculaBotellasPequenas(-2,3,17));
+		} catch (Exception e) {
+			return;
+	}
+	}
+	
+	@Test public void negativeLiters()
+	{
+		try {
+			Embotelladora test= new Embotelladora();
+			assertTrue("Error negativeLiters", -1==test.calculaBotellasPequenas(2,3,-17));
+		} catch (Exception e) {
+			return;
+		}
+	}
+	
+	@Test public void notEnoughLittle()
+	{
+		try {
+			Embotelladora test= new Embotelladora();
+			assertTrue("Error notEnoughLittle", -1==test.calculaBotellasPequenas(2,0,10));
+		} catch (Exception e) {
+			return;
+		}
+	}
+	
+}
